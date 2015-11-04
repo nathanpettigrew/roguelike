62605651effa99b6bc4de8f1bc7da665d59ea393
nathanpettigrew <nathanpettigrew223@gmail.com>
2015-11-04 18:52:10 +1030

    THAT MAP IS LOADING
    praise the sun

--- main.js
+++ main.js
@@ -48,12 +48,12 @@ var SCREEN_HEIGHT = canvas.height;
 
 //tile variables
 var TILE = 64;
-var MAP = {tw:102, th:64};
-var TILESET_TILE = TILE*2;
-var TILESET_PADDING = 2;
-var TILESET_COUNT_X = 14;
-var TILESET_COUNT_Y = 14;
-var LAYER_COUNT = 7;
+var MAP = {tw:64, th:64};
+var TILESET_TILE = 64;
+var TILESET_PADDING = 0;
+var TILESET_COUNT_X = 21;
+var TILESET_COUNT_Y = 36;
+var LAYER_COUNT = 2;
 var TILESET_SPACING = 0;
 
 //Collision constants
@@ -79,29 +79,29 @@ function intersects(x1, y1, w1, h1, x2, y2, w2, h2) {
 var worldOffsetX = 0;
 function drawMap()
 {
- for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
- {
- var idx = 0;
- for( var y = 0; y < level1.layers[layerIdx].height; y++ )
- {
- for( var x = 0; x < level1.layers[layerIdx].width; x++ )
- {
- if( level1.layers[layerIdx].data[idx] != 0 )
- {
- // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
- // correct tile
- var tileIndex = level1.layers[layerIdx].data[idx] - 1;
- var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
- var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
- context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
- }
- idx++;
- }
- }
- }
+for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+{
+var idx = 0;
+for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+{
+for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+{
+if( level1.layers[layerIdx].data[idx] != 0 )
+{
+// the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+// correct tile
+var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+}
+idx++;
+}
+}
+}
 }
 
-drawMap();
+
 
 function runSplash (deltaTime)
     {
@@ -115,6 +115,8 @@ function runGame()
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
 	var deltaTime = getDeltaTime();
+	
+	drawMap();
 	
 	player.update();
 	player.draw();
