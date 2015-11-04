28f64e0bcc0ca2b8bf5774c8d9d3db0daa5fd9f4
nathanpettigrew <nathanpettigrew223@gmail.com>
2015-11-04 18:04:05 +1030

    test

--- main.js
+++ main.js
@@ -53,6 +53,8 @@ var TILESET_TILE = TILE*2;
 var TILESET_PADDING = 2;
 var TILESET_COUNT_X = 14;
 var TILESET_COUNT_Y = 14;
+var LAYER_COUNT = 7;
+var TILESET_SPACING = 0;
 
 //Collision constants
 var METER = TILE;
@@ -60,6 +62,9 @@ var MAXDX = METER * 10;
 var MAXDY = METER * 15;
 var ACCEL = MAXDX * 2;
 var FRICTION = MAXDX * 6;
+
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
 
 function intersects(x1, y1, w1, h1, x2, y2, w2, h2) {
     if (y2 + h2 < y1 ||
@@ -74,47 +79,29 @@ function intersects(x1, y1, w1, h1, x2, y2, w2, h2) {
 var worldOffsetX = 0;
 function drawMap()
 {
-	var startX = -1;
-	var maxTiles = Math.floor(SCREEN_WIDTH / TILE) + 2;
-	var tileX = pixelToTile(player.position.x);
-	var offsetX = TILE + Math.floor(player.position.x%TILE);
-	
-	startX = tileX - Math.floor(maxTiles / 2);
-	
-	if(startX < -1)
-	{
-		startX = 0;
-		offsetX = 0;
-	}
-	
-	if(startX > MAP.tw - maxTiles)
-	{
-		startX = MAP.tw - maxTiles + 1;
-		offsetX = TILE;
-	}
-	
-	worldOffsetX = startX * TILE + offsetX;
-	
-	 for(var layerIdx=0; layerIdx < LAYER_COUNT; layerIdx++)
-	 {
-		 for( var y =0; y < level1.layers[layerIdx].height; y++ )
-		 {
-			 var idx = y * level1.layers[layerIdx].width + startX;
-			 for(var x = startX; x < startX + maxTiles; x++)
-			 {
-				 if( level1.layers[layerIdx].data[idx] !=0)
-				 {
-					 var tileIndex = level1.layers[layerIdx].data[idx] - 1;
-					 var sx = TILESET_PADDING + ( tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
-					 var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
-					 context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE,
-						(x-startX)*TILE - offsetX, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
-				 }
-				 idx++;
-			 }
-		 }
-	 }
+ for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+ {
+ var idx = 0;
+ for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+ {
+ for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+ {
+ if( level1.layers[layerIdx].data[idx] != 0 )
+ {
+ // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+ // correct tile
+ var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+ var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+ var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+ context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+ }
+ idx++;
+ }
+ }
+ }
 }
+
+drawMap();
 
 function runSplash (deltaTime)
     {
