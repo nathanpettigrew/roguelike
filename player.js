98e9e26f0bae05128365326428533bb8d9f79fc4
Kerrang888 <mitchell.bender@internode.on.net>
2015-11-14 10:13:06 +1030

    COLLISION WORKS!!!!!!!
    WOOOOOOOOOOOOOOOOO

--- player.js
+++ player.js
@@ -24,7 +24,12 @@ var Player = function()
 
 Player.prototype.update = function(deltaTime)
 {		
-
+	
+	var left = false;
+	var right = false;
+	var up = false;
+	var down = false;
+	
 	if(this.cooldownTimer > 0)
 	{
 		this.cooldownTimer -=deltaTime;
@@ -32,24 +37,50 @@ Player.prototype.update = function(deltaTime)
 
 	if(keyboard.isKeyDown(keyboard.KEY_W) == true)
 	{
-		this.position.y -= 4;
+		up = true;
 		this.direction = UP;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_A) == true)
 	{
-		this.position.x -= 4;
+		left = true;
 		this.direction = LEFT;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_S) == true)
 	{
-		this.position.y += 4;
+		down = true;
 		this.direction = DOWN;
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_D) == true)
 	{
-		this.position.x += 4;
+		right = true;
 		this.direction = RIGHT;
 	}
+	
+	var wasleft = this.velocity.x < 0;
+	var wasright = this.velocity.x > 0;
+	var wasup = this.velocity.y > 0;
+	var wasdown = this.velocity.y < 0;
+	var ddx = 0;
+	var ddy = 0;
+	
+	if (left)
+		ddx = ddx - ACCEL;
+	else if (wasleft)
+		ddx = ddx + FRICTION;
+	if (right)
+		ddx = ddx + ACCEL;
+	else if (wasright)
+		ddx = ddx - FRICTION;
+	
+	if (up)
+		ddy = ddy - ACCEL;
+	else if (wasup)
+		ddy = ddy + FRICTION;
+	
+	if (down)
+		ddy = ddy + ACCEL;
+	else if (wasdown)
+		ddy = ddy - FRICTION;
 	
 	if(keyboard.isKeyDown(keyboard.KEY_UP) == true && this.cooldownTimer <= 0)
 	{
@@ -86,8 +117,18 @@ Player.prototype.update = function(deltaTime)
 	// calculate the new position and velocity:
 	this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
 	this.position.x = Math.floor(this.position.x + (deltaTime * this.velocity.x));
-	//this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX); 
-	//this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
+	this.velocity.x = bound(this.velocity.x + (deltaTime * ddx), -MAXDX, MAXDX); 
+	this.velocity.y = bound(this.velocity.y + (deltaTime * ddy), -MAXDY, MAXDY);
+	
+	if ((wasleft && (this.velocity.x > 0)) || (wasright && (this.velocity.x < 0)))
+	{
+		this.velocity.x = 0;
+	}
+	
+	if ((wasup && (this.velocity.y > 0)) || (wasdown && (this.velocity.y < 0)))
+	{
+		this.velocity.y = 0;
+	}
 
 	// collision detection
 	// Our collision detection logic is greatly simplified by the fact that the player is a rectangle
@@ -103,6 +144,45 @@ Player.prototype.update = function(deltaTime)
 	var cellright = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty);
 	var celldown = cellAtTileCoord(LAYER_PLATFORMS, tx, ty + 1);
 	var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
+	
+	if (this.velocity.x > 0) 
+	{
+		 if ((cellright && !cell) || (celldiag && !celldown && ny)) {
+		 // clamp the x position to avoid moving into the platform we just hit
+		 this.position.x = tileToPixel(tx);
+		 this.velocity.x = 0; // stop horizontal velocity
+		 }
+	}
+	
+		else if (this.velocity.x < 0) 
+		{
+		 if ((cell && !cellright) || (celldown && !celldiag && ny)) {
+		// clamp the x position to avoid moving into the platform we just hit
+		this.position.x = tileToPixel(tx + 1);
+		this.velocity.x = 0; // stop horizontal velocity
+		 }
+	}
+	
+	if (this.velocity.y > 0) 
+	{
+		((celldown && !cell) || (celldiag && !cellright && nx))
+		{
+			this.position.y = tileToPixel(ty);
+			this.velocity.y = 0;
+			ny = 0;
+		}
+	}
+	else if (this.velocity.y < 0) 
+	{
+		if ((cell && celldown) || (cellright && !celldiag && nx))
+		{
+			this.position.y = tileToPixel(ty + 1);
+			this.velocity.y = 0;
+			cell = celldown;
+			cellright = celldiag;
+			ny = 0;
+		}
+	}
 
 }
 
