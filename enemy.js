92e423cc9660849ab0c09f73370e82bf8aa970d0
rowlYy-AIE <luke@rowlands.info>
2015-11-14 15:28:40 +1030

    Enemy Fixed

--- enemy.js
+++ enemy.js
@@ -18,7 +18,7 @@ Enemy.prototype.update = function(deltaTime)
 Enemy.prototype.draw = function()
 {
 	context.save();
-		context.translate(this.position.x, this.position.y);
+		context.translate(this.position.x - worldOffsetX, this.position.y - worldOffsetY);
 		context.drawImage(this.image, this.width - 64, this.height - 64);
 	context.restore();
 }
